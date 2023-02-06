bcb570a93069e44eb80dcb6fc3529eea7b63c31e
pauwma <pauwmaa@gmail.com>
2023-02-07 00:12:42 +0100

    Animación

--- src/main/java/com/example/stargo/Game.java
+++ src/main/java/com/example/stargo/Game.java
@@ -1,7 +1,10 @@
 package com.example.stargo;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -13,6 +16,7 @@ import javafx.stage.Stage;
 
 import javafx.animation.AnimationTimer;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,7 +39,6 @@ public class Game extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.drawImage(new Image("space.png"),0,0);
 
-
         asteroides = new ArrayList<>();
 
         for (int i = 0; i < 999; i++) {
@@ -46,7 +49,7 @@ public class Game extends Application {
                     Platform.runLater(new Runnable() {
                         @Override
                         public void run() {
-                            Asteroide asteroide = new Asteroide(new Image("asteroide_1.png"), 1);
+                            Asteroide asteroide = new Asteroide(new Image("asteroide_1.png"), 2);
                             asteroide.getImageView().setScaleX(0.18);
                             asteroide.getImageView().setScaleY(0.18);
                             root.getChildren().add(asteroide.getImageView());
@@ -56,14 +59,23 @@ public class Game extends Application {
                             asteroide.getImageView().setOnMouseClicked(new EventHandler<MouseEvent>() {
                                 @Override
                                 public void handle(MouseEvent event) {
-                                    root.getChildren().remove(asteroide.getImageView());
-                                    asteroides.remove(asteroide);
+                                    javafx.animation.ScaleTransition transition = new javafx.animation.ScaleTransition();
+                                    transition.setDuration(Duration.seconds(0.2f));
+                                    transition.setNode(asteroide.getImageView());
+                                    transition.setToX(0);
+                                    transition.setToY(0);
+                                    transition.setOnFinished(event1 -> {
+                                        root.getChildren().remove(asteroide.getImageView());
+                                        asteroides.remove(asteroide);
+                                    });
+                                    transition.play();
                                 }
                             });
+
                         }
                     });
                 }
-            }, i * 1000);
+            }, i * 800);
         }
 
 
